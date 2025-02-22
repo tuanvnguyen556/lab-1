ef1278bb8e6b25c416896f926d7462df21927e78
Tuan Nguyen <tuanvnguyen556@gmail.com>
2025-02-22 12:46:22 -0800

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 Days in Chicago</h1>
+      <h2>Welcome to Chicago, Natalia! Check out this calendar to get to know the city and see all the sights during your stay. &#127961;</h2>
+      <Calendar/>
+    </div>
+  )
+}
+
+export default App
